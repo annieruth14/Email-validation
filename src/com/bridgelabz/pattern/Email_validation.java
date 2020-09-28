519e577878fa3567a9e0d0a123bcadae7b112ffa
annie <ruthannie3@gmail.com>
2020-09-28 19:27:54 +0530

    [Annie]Added: Code to validate first mandatory part of an email

--- src/com/bridgelabz/pattern/Email_validation.java
+++ src/com/bridgelabz/pattern/Email_validation.java
@@ -1,8 +1,34 @@
 package com.bridgelabz.pattern;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Email_validation {
 	
+	public boolean checkPattern(String name) {
+		   boolean result = false;
+		   String pattern = "^[A-Za-z0-9]+$";
+		   
+		   // Create a Pattern object
+		   Pattern r = Pattern.compile(pattern);
+
+		   // Now create matcher object.
+		   Matcher m = r.matcher(name);
+		   if (m.find())
+			   	result = true;
+		   else 
+			   result = false;
+		   
+		   return result;
+	   }
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to email validation system");
+		Scanner sc = new Scanner(System.in);
+	       Email_validation obj = new Email_validation();
+	       System.out.println("Enter first mandatory part of an email");
+	       String value = sc.nextLine();
+	       Boolean ans = obj.checkPattern(value);
+	       System.out.println(ans);
 	}
 }
